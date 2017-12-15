5b65d61e37520574a49a5c4fc01047e4d3ebbb45
eric <ericjw@vt.edu>
2017-12-15 20:44:27 +0000

    moved flask out of AuthenticationPi bc authpi is broken

--- AuthenticationPi.py
+++ AuthenticationPi.py
@@ -5,10 +5,6 @@ import json
 from flask import make_response, request, Response, abort, Flask, jsonify
 from flask_discoverer import Discoverer, advertise
 
-#flask initialization
-app = Flask(__name__)
-discoverer = Discoverer(app)
-
 # MongoDB initialization
 try:
 	client = MongoClient('localhost', 27017)
@@ -74,62 +70,3 @@ for locker in lockers.find():
 	channel.basic_consume(, queue=locker['lockerID'], no_ack=True)
 
 channel.start_consuming()
-
-'''
-------------------------------Flask section-------------------------------------
-'''
-
-def check_auth(username, password):
-	"""
-	This function is called to check if a username /
-	password combination is valid.
-	"""
-	return True if username == "Apple" and password == "Pie" else False
-	
-def authenticate():
-	"""Sends a 401 response that enables basic auth"""
-	return Response(
-	'Could not verify your access level for that URL.\n'
-	'You have to login with proper credentials', 401,
-	{'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-def requires_auth(f):
-	@wraps(f)
-	def decorated(*args, **kwargs):
-		auth = request.authorization
-		if not auth or not check_auth(auth.username, auth.password):
-			return authenticate()
-		return f(*args, **kwargs)
-	return decorated
-	
-@app.route("/add_tag_user", methods =['POST'])
-@requires_auth
-def add_tag_user():
-	usr = request.get_json()['user']
-	tag = request.get_json()['tag']
-	print("usr: {}tag: {}".format(usr, tag))
-	#add tag to user here
-
-@app.route("/remove_tag_user", methods =['DELETE'])
-@requires_auth
-def remove_tag_user():
-	usr = request.get_json()['user']
-	tag = request.get_json()['tag']
-	#remove tag from user here
-	
-@app.route("/add_locker_user", methods =['POST'])
-@advertise()
-@requires_auth
-def add_locker_user():
-	usr = request.get_json()['user']
-	lock = request.get_json()['locker']
-	#add locker to user here
-
-@app.route("/remove_locker_user", methods =['DELETE'])
-@advertise()
-@requires_auth
-def remove_locker_user():
-	usr = request.get_json()['user']
-	lock = request.get_json()['locker']
-	#remove locker from user here
-
